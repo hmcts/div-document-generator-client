b5e8f67ea34460022b5cb8e18d2c374bde155aa5
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-03-16 11:11:13 +0000

    D8 Minipetition updated and special character handling

--- src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/util/HtmlFieldFormatter.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/util/HtmlFieldFormatter.java
@@ -3,27 +3,30 @@ package uk.gov.hmcts.reform.divorce.documentgenerator.util;
 import com.google.common.collect.ImmutableMap;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import org.springframework.web.util.HtmlUtils;
 
 import java.util.Map;
 
 @SuppressWarnings("squid:S1118")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class HtmlFieldFormatter {
-    private static final Map<String, String> CONVERSION_MAP = ImmutableMap.of("\n", "<br />");
+    private static final Map<Character, String> HTML_CHAR_ESCAPE_MAP =
+            ImmutableMap.of(
+                    '<', "&lt;",
+                    '>', "&gt;",
+                    '&', "&amp;",
+                    '"', "&quot;",
+                    '\n', "<br />"
+            );
 
     @SuppressWarnings("unchecked")
     public static <T> T format(T fieldData) {
         if (fieldData instanceof String) {
             String data = (String) fieldData;
 
-            data = HtmlUtils.htmlEscape(data);
+            //this is a temp hack to handle FE converting < to &lt; for security reason
+            data = data.replaceAll("&lt;", "<");
 
-            for (Map.Entry<String, String> entry : CONVERSION_MAP.entrySet()) {
-                data = data.replaceAll(entry.getKey(), entry.getValue());
-            }
-
-            return (T) data;
+            return (T)escape(data);
         }
 
         return fieldData;
@@ -35,16 +38,33 @@ public final class HtmlFieldFormatter {
             return fieldData;
         }
 
-        fieldData.entrySet()
-                .forEach(entry -> {
+        fieldData.entrySet().forEach(entry -> {
                     if (entry.getValue() instanceof Map) {
-                        entry.setValue(format((Map)entry.getValue()));
+                        entry.setValue(format((Map) entry.getValue()));
                     } else {
                         entry.setValue(format(entry.getValue()));
                     }
                 }
-            );
+        );
 
         return fieldData;
+    }
+
+    private static String escape(String string) {
+        StringBuilder builder = new StringBuilder();
+
+        for (char charToEscape : string.toCharArray()) {
+            String escapeString = HTML_CHAR_ESCAPE_MAP.get(charToEscape);
+
+            if (escapeString != null) {
+                builder.append(escapeString);
+            } else if (charToEscape < 128) {
+                builder.append(charToEscape);
+            } else {
+                builder.append("&#").append((int)charToEscape).append(";");
+            }
+        }
+
+        return builder.toString();
     }
 }
