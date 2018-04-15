44692b0f44bd1216836598ab517434a8eb0ec282
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-04-15 17:29:51 +0100

    clean up and context configuration

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/IntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/IntegrationTest.java
@@ -1,15 +1,13 @@
 package uk.gov.hmcts.reform.divorce;
 
 import net.serenitybdd.junit.spring.integration.SpringIntegrationMethodRule;
-import net.thucydides.core.annotations.WithTag;
-import net.thucydides.core.annotations.WithTags;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {TestContextConfiguration.class, ServiceContextConfiguration.class})
+@ContextConfiguration(classes = {ServiceContextConfiguration.class})
 public abstract class IntegrationTest {
 
     @Rule
