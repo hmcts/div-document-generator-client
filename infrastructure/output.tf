b20561f922bbc24edbea897f0311bdd49673b26c
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-05-03 13:31:01 +0100

    updated the dependency url

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -7,7 +7,7 @@ output "vaultUri" {
 }
 
 output "idam_s2s_url" {
-    value = "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.${local.ase_name}.internal"
+    value = "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.core-compute-${local.local_env}.internal"
 }
 
 output "test_environment" {
