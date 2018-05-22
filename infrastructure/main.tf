194c138dbf7ff159e770c7630d6ca3b0dc42febd
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-05-22 16:09:48 +0100

    send app insight to common location

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,13 +8,14 @@ locals {
 }
 
 module "div-dgs" {
-  source       = "git@github.com:hmcts/moj-module-webapp.git?ref=master"
-  product      = "${var.product}-${var.component}"
-  location     = "${var.location}"
-  env          = "${var.env}"
-  ilbIp        = "${var.ilbIp}"
-  subscription = "${var.subscription}"
-  is_frontend  = false
+  source                          = "git@github.com:hmcts/moj-module-webapp.git?ref=master"
+  product                         = "${var.product}-${var.component}"
+  location                        = "${var.location}"
+  env                             = "${var.env}"
+  ilbIp                           = "${var.ilbIp}"
+  subscription                    = "${var.subscription}"
+  appinsights_instrumentation_key = "${var.appinsights_instrumentation_key}"
+  is_frontend                     = false
 
   app_settings = {
     REFORM_SERVICE_NAME                                   = "${var.reform_service_name}"
