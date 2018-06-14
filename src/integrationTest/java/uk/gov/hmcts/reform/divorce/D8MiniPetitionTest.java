8050d373b40fad31988cefdd2f951916af071721
vakepati <37808224+vakepati@users.noreply.github.com>
2018-06-14 22:34:58 +0100

    Update D8MiniPetitionTest.java

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/D8MiniPetitionTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/D8MiniPetitionTest.java
@@ -48,8 +48,8 @@ public class D8MiniPetitionTest extends IntegrationTest {
                 {"CC--cores_FO--ChildApp_CN--BCDE_DR-AD-CRK-Yes-PL-Yes-DT-Yes_LP--Yes-Special-Chars-Confidential"},
                 {"CC--Res_FO--ChildApp_CN--CDEF_DR-UB-CRK-No-PL-No-DT-No_LP--No"},
                 {"CC--No_FO--App_CN--EF_DR-S2Y-CRK-No-PL-No-DT-No_LP--No"},
-                {"CC--No_FO--Child_CN--BCF_DR-S5Y-CRK-No-PL-No-DT-No_LP--No"}
-              //  {"CC--No_FO--ChildApp_CN--ABCDEFG_DR-DES-CRK-No-PL-No-DT-No_LP--No"}
+                {"CC--No_FO--Child_CN--BCF_DR-S5Y-CRK-No-PL-No-DT-No_LP--No"},
+                {"CC--No_FO--ChildApp_CN--ABCDEFG_DR-DES-CRK-No-PL-No-DT-No_LP--No"}
         });
     }
 
