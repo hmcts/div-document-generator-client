def38ad318a6dab51c28f8a4a58d74ff5b8f0238
tony.chow <tony.chow@hmcts.net>
2018-08-09 17:02:07 +0100

    Apply review comments

--- src/test/java/uk/gov/hmcts/reform/divorce/documentgenerator/controller/RootControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/documentgenerator/controller/RootControllerTest.java
@@ -4,21 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import uk.gov.hmcts.reform.divorce.documentgenerator.DocumentGeneratorApplication;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(RootController.class)
-@ContextConfiguration(classes = DocumentGeneratorApplication.class)
+@SpringBootTest
 @AutoConfigureMockMvc
 public class RootControllerTest {
 
