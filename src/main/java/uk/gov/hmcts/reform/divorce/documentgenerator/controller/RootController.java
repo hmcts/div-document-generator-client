4d4dab011fdb8abbef93e70bbf38c8128c1b1f50
tony.chow <tony.chow@hmcts.net>
2018-08-09 09:58:44 +0100

    fix-import-order

--- src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/controller/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/controller/RootController.java
@@ -2,8 +2,8 @@ package uk.gov.hmcts.reform.divorce.documentgenerator.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
 
