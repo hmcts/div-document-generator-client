d37b3bb50eeb2c8943acec0e427a12a700062e93
Mathan Thava <mathan.thavachelvam@hmcts.net>
2018-08-13 11:44:10 +0100

    point DGS to preview emca

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,8 @@ locals {
   ase_name = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
 
-  evidence_management_client_api_url = "http://${var.evidence_management_client_api_url_part}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  #evidence_management_client_api_url = "http://${var.evidence_management_client_api_url_part}-${local.local_env}.service.core-compute-${local.local_env}.internal"
+  evidence_management_client_api_url = "pr-62-div-emca-preview.service.core-compute-preview.internal"
   pdf_service_url                    = "http://${var.pdf_service_url_part}-${local.local_env}.service.core-compute-${local.local_env}.internal"
   idam_s2s_url                       = "http://${var.idam_s2s_url_prefix}-${local.local_env}.service.core-compute-${local.local_env}.internal"
 }
