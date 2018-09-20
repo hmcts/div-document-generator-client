d66ab370c20e8e81a434df907596453f22ec51bd
Tony Chow <36265570+tchow8@users.noreply.github.com>
2018-09-20 09:30:12 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,7 +13,7 @@ locals {
   vaultUri = "${data.azurerm_key_vault.div_key_vault.vault_uri}"
     
   asp_name = "${var.env == "prod" ? "div-dgs-prod" : "${var.product}-${var.env}"}"
-  asp_rg = "${var.env == "prod" ? "div-dgs-prod" : "${var.product}-shared-infrastructure-${var.env}"}"
+  asp_rg = "${var.env == "prod" ? "div-dgs-prod" : "${var.product}-${var.env}"}"
 }
 
 module "div-dgs" {
