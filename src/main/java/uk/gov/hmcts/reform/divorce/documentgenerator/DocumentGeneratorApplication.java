2783f803bed2bf672f1d31b1f730167d7decc877
qzhou-hmcts <42572376+qzhou-hmcts@users.noreply.github.com>
2019-04-12 13:02:55 +0100

    DIV-4519 Added helm chart config (#113)

--- src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/DocumentGeneratorApplication.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/DocumentGeneratorApplication.java
@@ -2,11 +2,11 @@ package uk.gov.hmcts.reform.divorce.documentgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import uk.gov.hmcts.reform.authorisation.healthcheck.ServiceAuthHealthIndicator;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthAutoConfiguration;
 
 @SpringBootApplication(
     scanBasePackages = {"uk.gov.hmcts.reform.divorce",  "uk.gov.hmcts.reform.logging.appinsights"},
-    exclude = {ServiceAuthHealthIndicator.class})
+    exclude = {ServiceAuthAutoConfiguration.class})
 public class DocumentGeneratorApplication {
 
     public static void main(String[] args) {
