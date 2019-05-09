5eb3509764fe4e31362fe4efceef22dcf253e61b
tony.chow <tony.chow@hmcts.net>
2019-05-09 12:54:35 +0100

    Modify domosis unit tests to be picked up Sonar

--- src/test/java/uk/gov/hmcts/reform/divorce/documentgenerator/service/impl/DocmosisPDFGenerationServiceImplUTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/documentgenerator/service/impl/DocmosisPDFGenerationServiceImplUTest.java
@@ -6,8 +6,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
-import org.powermock.api.support.membermodification.MemberMatcher;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -15,22 +13,22 @@ import org.powermock.reflect.Whitebox;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.divorce.documentgenerator.config.DocmosisBasePdfConfig;
 import uk.gov.hmcts.reform.divorce.documentgenerator.domain.request.PdfDocumentRequest;
 import uk.gov.hmcts.reform.divorce.documentgenerator.exception.PDFGenerationException;
 import uk.gov.hmcts.reform.divorce.documentgenerator.util.NullOrEmptyValidator;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-import static org.powermock.api.mockito.PowerMockito.doNothing;
 import static org.powermock.api.mockito.PowerMockito.doReturn;
 import static org.powermock.api.mockito.PowerMockito.doThrow;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
 
@@ -39,14 +37,19 @@ import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 @PrepareForTest({DocmosisPDFGenerationServiceImpl.class, NullOrEmptyValidator.class, Objects.class})
 public class DocmosisPDFGenerationServiceImplUTest {
 
+    private static final String CASE_DETAILS = "caseDetails";
+    private static final String CASE_DATA = "case_data";
+
     private static final String PDF_SERVICE_ENDPOINT = "pdf_service_endpoint";
     private static final String PDF_SERVICE_KEY = "pdf_service_key" ;
+
+    @Mock
+    private DocmosisBasePdfConfig docmosisBasePdfConfig;
 
     @Mock
     private RestTemplate restTemplate;
 
     @InjectMocks
-    @Spy
     private final DocmosisPDFGenerationServiceImpl classUnderTest = new DocmosisPDFGenerationServiceImpl();
 
     @Before
@@ -57,27 +60,29 @@ public class DocmosisPDFGenerationServiceImplUTest {
     }
 
     @Test
-    public void givenHttpClientErrorExceptionThrown_whenGenerateCalled_thenThrowPDFGenerationException()
-        throws Exception {
+    public void givenHttpClientErrorExceptionThrown_whenGenerateCalled_thenThrowPDFGenerationException() {
         final String template = "1";
-        final Map<String, Object> placeholders = Collections.emptyMap();
+        final Map<String, Object> dataMap = new HashMap<>();
+        final Map<String, Object> caseData = Collections.singletonMap(CASE_DATA, dataMap);
+        final Map<String, Object> placeholders = Collections.singletonMap(CASE_DETAILS, caseData);
         final HttpClientErrorException httpClientErrorException = mock(HttpClientErrorException.class);
 
-        doNothing().when(NullOrEmptyValidator.class);
-        NullOrEmptyValidator.requireNonBlank(template);
-        doReturn(placeholders).when(Objects.class);
-        Objects.requireNonNull(placeholders);
+        Map<String, Object> expectedPlaceholders = new HashMap<>();
+        expectedPlaceholders.put(docmosisBasePdfConfig.getDisplayTemplateKey(),
+            docmosisBasePdfConfig.getDisplayTemplateVal());
+        expectedPlaceholders.put(docmosisBasePdfConfig.getFamilyCourtImgKey(),
+            docmosisBasePdfConfig.getFamilyCourtImgVal());
+        expectedPlaceholders.put(docmosisBasePdfConfig.getHmctsImgKey(), docmosisBasePdfConfig.getHmctsImgVal());
+
         PdfDocumentRequest pdfDocumentRequest = PdfDocumentRequest.builder()
             .accessKey(PDF_SERVICE_KEY)
-            .data(placeholders)
+            .data(expectedPlaceholders)
             .templateName(template)
+            .outputName("result.pdf")
             .build();
-        doReturn(pdfDocumentRequest).when(classUnderTest,
-            MemberMatcher.method(DocmosisPDFGenerationServiceImpl.class, "request",
-                String.class, Map.class))
-            .withArguments(Mockito.anyString(), Mockito.any(Map.class));
-        doThrow(httpClientErrorException).when(restTemplate).postForEntity(PDF_SERVICE_ENDPOINT,
-            pdfDocumentRequest, byte[].class);
+
+        doThrow(httpClientErrorException).when(restTemplate)
+            .postForEntity(PDF_SERVICE_ENDPOINT, pdfDocumentRequest, byte[].class);
 
         try {
             classUnderTest.generate(template, placeholders);
@@ -86,41 +91,38 @@ public class DocmosisPDFGenerationServiceImplUTest {
             assertEquals(httpClientErrorException, exception.getCause());
         }
         NullOrEmptyValidator.requireNonBlank(template);
-        verifyPrivate(classUnderTest, Mockito.times(1))
-            .invoke("request", template, placeholders);
     }
 
     @Test
-    public void givenHttpRequestGoesThrough_whenGenerateFromHtml_thenReturnProperResponse() throws Exception {
+    public void givenHttpRequestGoesThrough_whenGenerateFromHtml_thenReturnProperResponse() {
         final String template = "1";
-        final Map<String, Object> placeholders = Collections.emptyMap();
-        final HttpClientErrorException httpClientErrorException = mock(HttpClientErrorException.class);
+        final Map<String, Object> dataMap = new HashMap<>();
+        final Map<String, Object> caseData = Collections.singletonMap(CASE_DATA, dataMap);
+        final Map<String, Object> placeholders = Collections.singletonMap(CASE_DETAILS, caseData);
 
-        doNothing().when(NullOrEmptyValidator.class);
-        NullOrEmptyValidator.requireNonBlank(template);
-        doReturn(placeholders).when(Objects.class);
-        Objects.requireNonNull(placeholders);
+        Map<String, Object> expectedPlaceholders = new HashMap<>();
+        expectedPlaceholders.put(docmosisBasePdfConfig.getDisplayTemplateKey(),
+            docmosisBasePdfConfig.getDisplayTemplateVal());
+        expectedPlaceholders.put(docmosisBasePdfConfig.getFamilyCourtImgKey(),
+            docmosisBasePdfConfig.getFamilyCourtImgVal());
+        expectedPlaceholders.put(docmosisBasePdfConfig.getHmctsImgKey(), docmosisBasePdfConfig.getHmctsImgVal());
+
         PdfDocumentRequest pdfDocumentRequest = PdfDocumentRequest.builder()
             .accessKey(PDF_SERVICE_KEY)
-            .data(placeholders)
+            .data(expectedPlaceholders)
             .templateName(template)
+            .outputName("result.pdf")
             .build();
-        doReturn(pdfDocumentRequest).when(classUnderTest,
-            MemberMatcher.method(DocmosisPDFGenerationServiceImpl.class, "request",
-                String.class, Map.class))
-            .withArguments(Mockito.anyString(), Mockito.any(Map.class));
+
         byte[] pdfBytes = "output".getBytes();
         ResponseEntity responseEntity = Mockito.mock(ResponseEntity.class);
         doReturn(pdfBytes).when(responseEntity).getBody();
         doReturn(responseEntity).when(restTemplate).postForEntity(PDF_SERVICE_ENDPOINT,
             pdfDocumentRequest, byte[].class);
 
-        try {
-            byte[] bytes = classUnderTest.generate(template, placeholders);
-            assertEquals(pdfBytes, bytes);
-        } catch (PDFGenerationException exception) {
-            assertEquals(httpClientErrorException, exception.getCause());
-        }
+        byte[] bytes = classUnderTest.generate(template, placeholders);
+        assertEquals(pdfBytes, bytes);
+
         NullOrEmptyValidator.requireNonBlank(template);
         verifyStatic(Objects.class);
     }
