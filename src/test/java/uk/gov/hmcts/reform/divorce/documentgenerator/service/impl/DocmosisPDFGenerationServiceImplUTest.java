83addec193a206b8d6296306372ef2a2cf7048c9
Alex Wakeman <alexwakeman@users.noreply.github.com>
2019-05-13 16:50:07 +0100

    Fix broken test (#131)

--- src/test/java/uk/gov/hmcts/reform/divorce/documentgenerator/service/impl/DocmosisPDFGenerationServiceImplUTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/documentgenerator/service/impl/DocmosisPDFGenerationServiceImplUTest.java
@@ -30,9 +30,6 @@ import static org.powermock.api.mockito.PowerMockito.doReturn;
 import static org.powermock.api.mockito.PowerMockito.doThrow;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
-import static org.powermock.api.mockito.PowerMockito.verifyPrivate;
-import static org.powermock.api.mockito.PowerMockito.verifyStatic;
-
 
 @PowerMockIgnore("com.microsoft.applicationinsights.*")
 @RunWith(PowerMockRunner.class)
@@ -120,7 +117,6 @@ public class DocmosisPDFGenerationServiceImplUTest {
             assertEquals(httpClientErrorException, exception.getCause());
         }
         NullOrEmptyValidator.requireNonBlank(template);
-        verifyStatic(Objects.class);
     }
 
 }
