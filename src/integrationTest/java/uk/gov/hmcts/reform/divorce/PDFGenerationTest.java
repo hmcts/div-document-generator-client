a4cd5922c1b646d001afa6fc4be1ae0dd83bbb2a
tony.chow <tony.chow@hmcts.net>
2019-06-28 07:09:30 +0100

    Fixed integrationTest notification payload

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/PDFGenerationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/PDFGenerationTest.java
@@ -9,7 +9,6 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 
 import java.io.File;
