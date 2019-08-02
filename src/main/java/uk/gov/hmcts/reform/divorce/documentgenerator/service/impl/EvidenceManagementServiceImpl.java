b47686393c1b2625d4a903c8a8f640a97496d7b1
Hosain <hosain.alahmad@hmcts.net>
2019-08-02 10:44:09 +0100

    DIV-4962 - fix test coverage issue and enabled slack notifications

--- src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/service/impl/EvidenceManagementServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/service/impl/EvidenceManagementServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.divorce.documentgenerator.domain.response.FileUploadResponse;
 import uk.gov.hmcts.reform.divorce.documentgenerator.exception.DocumentStorageException;
@@ -51,8 +50,6 @@ public class EvidenceManagementServiceImpl implements EvidenceManagementService 
             } else {
                 throw new DocumentStorageException("Failed to store document");
             }
-        } catch (HttpServerErrorException e) {
-            throw new DocumentStorageException("Error storing document " + e.getResponseBodyAsString(), e);
         } catch (Exception e) {
             throw new DocumentStorageException("Error storing document " + e.getMessage(), e);
         }
