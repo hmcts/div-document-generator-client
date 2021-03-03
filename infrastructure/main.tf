e0e004ecf5f5076d5995c9b8774afd9128de5970
Muhammad Uzair Baig <muhammad.baig@hmcts.net>
2021-03-03 15:26:07 +0000

    content changes for aos template

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -24,6 +24,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_storage_account" "div_file_storage" {
+    name                     = "divdocstorage"
+    resource_group_name      = azurerm_resource_group.rg.name
+    location                 = azurerm_resource_group.rg.location
+    account_tier             = "Standard"
+    account_replication_type = "GRS"
+}
+
 data "azurerm_key_vault" "div_key_vault" {
     name                = local.vaultName
     resource_group_name = local.vaultName
