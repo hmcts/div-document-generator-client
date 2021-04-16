6e340577c9adf9c3f3d2674bde3221fb05aa0741
Ruban <ruban72@hotmail.com>
2021-04-16 12:36:35 +0100

    TA-451: Set up local env

--- src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/management/test/stub/impl/EvidenceManagementServiceStub.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/management/test/stub/impl/EvidenceManagementServiceStub.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.documentgenerator.management.test.stub.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,25 +15,29 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
+@Slf4j
 @ConditionalOnProperty(value = "evidence-management-api.service.stub.enabled", havingValue = "true")
 public class EvidenceManagementServiceStub implements EvidenceManagementService, DocumentDownloadService {
     private static final String TEST_DOCUMENTS_DOWNLOAD_CONTEXT_PATH = "/test/documents/";
 
     private static final Map<String, byte[]> DATA_STORE = new ConcurrentHashMap<>();
     private static final String CREATED_BY = "Document Generator Service";
+    private static Integer counter = 0;
 
     private final Clock clock = Clock.systemDefaultZone();
 
     @Override
     public FileUploadResponse storeDocumentAndGetInfo(byte[] document, String authorizationToken, String fileName) {
+        fileName = fileName.replace(".","").concat((counter++).toString());
         DATA_STORE.put(fileName, document);
-
+        log.info("File stored as {}", fileName);
         FileUploadResponse fileUploadResponse = new FileUploadResponse(HttpStatus.OK);
         fileUploadResponse.setFileUrl(getFileURL(fileName));
         fileUploadResponse.setMimeType(MediaType.APPLICATION_PDF_VALUE);
         fileUploadResponse.setCreatedOn(clock.instant().toString());
         fileUploadResponse.setCreatedBy(CREATED_BY);
-
+        log.info("File response created with url: {} and filename: {} ",
+            fileUploadResponse.getFileUrl(),fileUploadResponse.getFileName());
         return fileUploadResponse;
     }
 
