b0eb7ec07b240ba1d7798bb2ff524c1f2155018e
Alan <78535152+alanwilliamsmoj@users.noreply.github.com>
2021-07-07 11:05:55 +0100

    DFR-101 : Always close resource (#482)
    DFR-101 : Always close resource

--- src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/util/ResourceLoader.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/util/ResourceLoader.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import org.apache.commons.io.IOUtils;
 import uk.gov.hmcts.reform.divorce.documentgenerator.exception.ErrorLoadingTemplateException;
 
+import java.io.InputStream;
+
 @SuppressWarnings("squid:S1118")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ResourceLoader {
@@ -12,8 +14,8 @@ public final class ResourceLoader {
     public static byte[] loadResource(String path) {
         NullOrEmptyValidator.requireNonBlank(path);
 
-        try {
-            return IOUtils.toByteArray(ResourceLoader.class.getClassLoader().getResourceAsStream(path));
+        try (InputStream inputStream = ResourceLoader.class.getClassLoader().getResourceAsStream(path)) {
+            return IOUtils.toByteArray(inputStream);
         } catch (Exception e) {
             throw new ErrorLoadingTemplateException(String.format("Couldn't load template with the name %s", path), e);
         }
