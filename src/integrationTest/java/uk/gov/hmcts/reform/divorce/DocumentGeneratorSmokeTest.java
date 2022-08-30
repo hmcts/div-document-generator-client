d60dfac8653571d777d1d4513e186b88448432bb
KleoG <kleogeorgiou@hotmail.com>
2022-08-30 10:34:08 +0100

    bring back smoke health check test

--- src/integrationTest/java/uk/gov/hmcts/reform/divorce/DocumentGeneratorSmokeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/divorce/DocumentGeneratorSmokeTest.java
@@ -5,16 +5,17 @@ import org.junit.experimental.categories.Category;
 import uk.gov.hmcts.reform.divorce.documentgenerator.category.SmokeTest;
 
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.is;
 
 @Category(SmokeTest.class)
 public class DocumentGeneratorSmokeTest extends IntegrationTest {
 
     @Test
     public void shouldHaveHealthyService() {
-        when().get(divDocumentGeneratorBaseURI + "/health");
-        //.then()
-        //.statusCode(200)
-        //.body("status", is("UP"));
+        when().get(divDocumentGeneratorBaseURI + "/health")
+        .then()
+        .statusCode(200)
+        .body("status", is("UP"));
     }
 
 }
