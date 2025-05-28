7fe63ef14243538aff31ec99b31472b67da261b7
Ashley Wong <im@so99y.com>
2025-05-28 12:52:11 +0100

    Remove springfox and migrate to springdoc

--- src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/domain/request/GenerateDocumentRequest.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/documentgenerator/domain/request/GenerateDocumentRequest.java
@@ -1,21 +1,20 @@
 package uk.gov.hmcts.reform.divorce.documentgenerator.domain.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 
 import java.util.Map;
 import javax.validation.constraints.NotBlank;
 
 @Value
-@ApiModel(description = "Request body model for Document Generation Request")
+@Schema(description = "Request body model for Document Generation Request")
 public class GenerateDocumentRequest {
-    @ApiModelProperty(value = "Name of the template", required = true)
+    @Schema(description = "Name of the template", required = true, example = "divorce-template")
     @JsonProperty(value = "template", required = true)
     @NotBlank
-    private final String template;
+    String template;
     @JsonProperty(value = "values", required = true)
-    @ApiModelProperty(value = "Placeholder key / value pairs", required = true)
-    private final Map<String, Object> values;
+    @Schema(description = "Placeholder key / value pairs", required = true)
+    Map<String, Object> values;
 }
